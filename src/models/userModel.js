4935ec704e5dbf443bdbd12d77e8efd023462e11
saifmalik2004 <saifmalik7827@gmail.com>
2024-07-25 13:53:41 +0530

    check

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -88,5 +88,5 @@ userSchema.methods.generateRefreshToken = function(){
             expiresIn: process.env.REFRESH_TOKEN_EXPIRY
         }
     )
-}
+    }
 export const User = mongoose.model("User",userSchema)
