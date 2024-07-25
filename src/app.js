919ade7971f38612a5ea5f1e340b0dc23b5f0cbf
saifmalik2004 <saifmalik7827@gmail.com>
2024-07-25 16:54:40 +0530

    setup config

--- src/app.js
+++ src/app.js
@@ -13,4 +13,11 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser())
+
+//routes
+import userRouter from './routes/user.routes.js'
+
+//route declartion
+app.use("/api/v1/users", userRouter);
+
 export default app
